3f0ff86b45ae30cad72ca026f7fab8c83feb1b50
Herbert Hudson <herbert.trindade@businessintegrator.com.br>
2019-10-14 14:48:44 -0300

    Iniciando classe de consulta

---
+++ Receita/ConsultaEmpresa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Receita
+{
+    interface IConsultaEmpresa
+    {
+        Empresa Consultar();
+        Empresa Consultar(Empresa empresa);
+        Empresa Consultar(string url);
+        Empresa Consultar(Empresa empresa, string url);
+    }
+}
---
+++ Receita/ReceitaWS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Receita
+{
+    class ReceitaWS : IConsultaEmpresa
+    {
+        private Empresa empresa;
+
+        public Empresa Consultar()
+        {
+            empresa = new Empresa();
+
+            return empresa;
+        }
+
+        public Empresa Consultar(Empresa _empresa)
+        {
+            return empresa = _empresa;
+        }
+
+        public Empresa Consultar(string url)
+        {
+            return empresa;
+        }
+
+        public Empresa Consultar(Empresa _empresa, string url)
+        {
+
+            return empresa;
+        }
+    }
+}
